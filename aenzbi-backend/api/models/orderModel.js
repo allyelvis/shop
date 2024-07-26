34a6c9d25e86d8e854e3a46b598d03ee08518268
Ally Elvis Nzeyimana <155692151+allyelvis@users.noreply.github.com>
2024-07-27 01:03:40 +0200

    Create orderModel.js
    Define the schema of orders

---
+++ aenzbi-backend/api/models/orderModel.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose');
+
+const orderSchema = new mongoose.Schema({
+    items: [{ productId: { type: mongoose.Schema.Types.ObjectId, ref: 'Product' }, quantity: Number }],
+    total: { type: Number, required: true },
+    status: { type: String, default: 'Pending' },
+    createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('Order', orderSchema);
